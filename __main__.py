de8fc473cd1985d4c78938883768557959ff68f6
Julien-Sklarik <julien.sklarik@eleve.ensai.fr>
2021-09-11 14:24:59 +0200

    test
    commit

--- __main__.py
+++ __main__.py
@@ -1,5 +1,9 @@
 #Test2
 import psycopg2
+from flask import Flask
+from flask_restful import Resource, Api
+
+
 """
 # Connect to your postgres DB
 conn = psycopg2.connect("dbname=test user=postgres")
@@ -13,3 +17,15 @@ cur.execute("SELECT * FROM my_data")
 # Retrieve query results
 records = cur.fetchall()
 """
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
