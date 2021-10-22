808d53ac56f0a5b2d5f74baf314aa6a01787a5ca
Your Name <you@example.com>
2021-10-22 12:10:37 +0200

    tout

--- __main__.py
+++ __main__.py
@@ -1,10 +1,33 @@
-from lancerDe import LancerDe
-import requests as req
-import json
+#Test2
+import psycopg2
+from flask import Flask
+from flask_restful import Resource, Api
 
-D=LancerDe("Moi",2,6)
-print(D)
-r = req.get('https://www.dnd5eapi.co/api/monsters/adult-black-dragon/')
-d=r.json()
 
-r2 = req.get()
+"""
+# Connect to your postgres DB
+conn = psycopg2.connect("dbname=test user=postgres")
+
+# Open a cursor to perform database operations
+cur = conn.cursor()
+
+# Execute a query
+cur.execute("SELECT * FROM my_data")
+
+# Retrieve query results
+records = cur.fetchall()
+"""
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+#test
