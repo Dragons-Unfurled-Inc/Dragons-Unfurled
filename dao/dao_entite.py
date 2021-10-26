beb1fcbc357edc7109b47a3da0674e0eb56d494e
Your Name <you@example.com>
2021-10-26 12:30:51 +0200

    fix

--- dao/dao_entite.py
+++ dao/dao_entite.py
@@ -2,11 +2,12 @@ from objets_metier.entite import Entite
 from objets_metier.monstre import Monstre 
 import requests as req 
 from abc import abstractstaticmethod
+
+#le code est pas ouf mais vous avez une idée de comment faire, par contre c'est ptet plus à sa place dans le package web
+
 class CreationMonstre:
     @staticmethod
-    def CreaMonstre(nom):
-        r = req.get('https://www.dnd5eapi.co/api/monsters/aboleth')
+    def CreaMonstre(nom): 
+        r = req.get('https://www.dnd5eapi.co/api/monsters/aboleth') 
         d=r.json()
-        print(d.keys())
-        print(d.keys())
         return(Monstre(nom,d["size"],d["alignment"],d['armor_class'],d['hit_points'],d['hit_dice'],d['speed'],d['strength'],d['dexterity'],d['constitution'],d['intelligence'],d['wisdom'],d['charisma'],d['proficiencies'],d['languages'],d['xp']))
--- objets_metier/entite.py
+++ objets_metier/entite.py
@@ -4,7 +4,7 @@ from objets_metier.caracteristique import Caracteristique
 
 
 class Entite(ABC): 
-    def __init__(self, id_joueur, id_entite, nom_entite, objets, caracteristiques = Caracteristique()): #faut trouver un moyen d'initialiser des caracs à chaque fois
+    def __init__(self, id_joueur, id_entite, nom_entite, objets, caracteristiques): #faut trouver un moyen d'initialiser des caracs à chaque fois
         self.id_joueur = id_joueur
         self.id_entite = id_entite  
         self.nom_entite = nom_entite
