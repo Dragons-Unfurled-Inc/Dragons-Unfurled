1c2a01f14d7246e9298ec2ed8a6db6b7cac3eefe
Your Name <you@example.com>
2021-10-26 20:34:36 +0200

    fix

---
+++ service/personnage_service.py
@@ -0,0 +1,36 @@
+from objets_metier.personnage import Personnage 
+
+'''
+Experience Points	Level	Proficiency Bonus
+0   1	+2
+300	2	+2
+900	3	+2
+2.700	4	+2
+6.500	5	+3
+14.000	6	+3
+23.000	7	+3
+34.000	8	+3
+48.000	9	+4
+64.000	10	+4
+85.000	11	+4
+100.000	12	+4
+120.000	13	+5
+140.000	14	+5
+165.000	15	+5
+195.000	16	+5
+225.000	17	+6
+265.000	18	+6
+305.000	19	+6
+355.000	20	+6 '''
+points = [0, 300,900,2.700	,6.500	,14.000	,23.000	,34.000	,48.000	,64.000	,85.000	,100.000,120.000,140.000,165.000,195.000,225.000, 265.000,	305.000, 355.000]	
+#Ceci est à stocker dans notre API ! 
+
+class PersonnageService : 
+    
+    @staticmethod #après avoir posté ces données sur l'API on peut vérifier si avec un tel pt d'expérience un perso peut monter de niveau (ici version test)
+    def montee_niveau(personnage: Personnage) -> None: 
+        
+        
+
+  
+      
