fc03591ffd86affc5cc9adaf61f6f1887f9f90ab
Julien-Sklarik <julien.sklarik@eleve.ensai.fr>
2021-11-02 18:53:11 +0100

    view

--- client/view/abstract_view.py
+++ client/view/abstract_view.py
@@ -4,11 +4,13 @@ from client.view.session import Session
 
 
 class AbstractView(ABC):
+    """
+    Cette classe est la classe mère de toutes nos vues."""
 
     @abstractmethod
-    def display_info(self):
+    def display_info(self): # détermine l'affichage en console
         pass
 
     @abstractmethod
-    def make_choice(self):
+    def make_choice(self): # gère les choix de l'utilisateur et l'envoie vers une autre page
         pass
