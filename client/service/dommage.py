18c0b9a59a3608f9702f302fbc1592f056179653
Julien-Sklarik <julien.sklarik@eleve.ensai.fr>
2021-11-02 19:04:44 +0100

    dom

---
+++ client/service/dommage.py
@@ -0,0 +1,8 @@
+from objets_metier.entite import Entite
+from objets_metier.jet import Jet
+
+
+class Dommage : 
+    """
+    Cette classe permet de gérer automatiquement les dégats suite à une attaque d'une entité par une autre"""
+    #frappe(jet: Jet, attaquant: Entite, cible: Entite): str
