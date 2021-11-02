b9a29606c68e93ba196d775f9f615f22ea0869f8
evainmanon <evain.manon@orange.fr>
2021-11-02 15:15:45 +0100

    Classe Des explications

--- objets_metier/des.py
+++ objets_metier/des.py
@@ -2,7 +2,9 @@ from random import randint
 
 class Des():
     """
-    
+    Implémentation de la classe Des, un dés est définit par son nombre de face.
+    En général, les faces de dés possibles sont : 4, 6, 8, 10, 12, 20, 100. 
+    Par exemple, un dés comprenant 10 faces est numéroté de 1 à 10
     """
     def __init__(self, nb_face : int):
         self.__nb_face = nb_face
