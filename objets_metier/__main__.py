e726a96df75af58f6c5700765996e8c127512a47
evainmanon <evain.manon@orange.fr>
2021-11-02 17:17:57 +0100

    Création perso

--- objets_metier/__main__.py
+++ objets_metier/__main__.py
@@ -1,4 +1,8 @@
 from objets_metier.personnage import Personnage
+from objets_metier.caracteristique import Caracteristique
+from objets_metier.objet import Objet
 
-essai = Personnage("classe","race","lore","id_joueur", "id_entite","nom_entite","caracteristiques","objets")
+caract = Caracteristique("nom_entite", ["attack"], ["capacité"], ["languages"], "description")
+obj = Objet("id_object", "nom_objet", "descr_objet")
+essai = Personnage("classe","race","lore","id_joueur", "id_entite",caract,obj)
 print(essai)
