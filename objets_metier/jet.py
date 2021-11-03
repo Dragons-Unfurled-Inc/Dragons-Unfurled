ee63523dc55e1d04316f75bdfcf3aee758e8fca7
Julien-Sklarik <julien.sklarik@eleve.ensai.fr>
2021-11-03 13:03:24 +0100

    des

--- objets_metier/jet.py
+++ objets_metier/jet.py
@@ -1,73 +1,15 @@
-import random as rd
 from typing import List
 
 from objets_metier.des import Des
 
+    
 class Jet:
-    def __init__(self, lanceur, 
-                       nombre,
-                       faces,
-                       cible) -> None:
-        self.__lanceur = lanceur
-        self.__nombre = nombre 
-        self.__faces = faces
-        self.__cible = cible 
-        self.__resultat = nombre*[0]
-        for i in range(nombre) :
-            self.__resultat[i] = rd.randrange(1,faces)
-        
-    def __str__(self):
-    
-        """
+    def __init__(self, liste_des: List[Des]):
+        self.__liste_des = liste_des
+        self.__valeur_jet = None
 
-        permet un affichage du lancer de dés
-
-        """
-        modele = '\n'.join([
-            ' Le joueur  {} a lancé  {}  dé(s) à  {} faces et obtenu  {}'])
-        return modele.format(
-            self.__lanceur,
-            self.__nombre,
-            self.__faces,
-            self.__resultat)
-
-    def attaquer(self):
-        None
-    
-    def lancer_des(liste_des : List[Des]):
-        resultat = 0 
-        for i in range(0, len(liste_des)):
-            resultat += Des.lancer_un_des(liste_des[i])
-        return resultat
-
-    @property
-    def lanceur(self):
-        return self.__lanceur
-    
-    @lanceur.setter
-    def lanceur(self, value):
-        self.__lanceur = value
-
-    @property
-    def nombre(self):
-        return self.__nombre
-    
-    @nombre.setter
-    def nombre(self, value):
-        self.__nombre = value
-
-    @property
-    def faces(self):
-        return self.__faces
-    
-    @faces.setter
-    def faces(self, value):
-        self.__faces = value
-
-    @property
-    def resultat(self):
-        return self.__resultat
-    
-    @resultat.setter
-    def resultat(self, value):
-        self.__resultat = value
+    def lancer_des(self):
+        self.__valeur_jet = 0
+        longueur_liste_des = len(self.__liste_des)
+        for i in range(0, longueur_liste_des):
+            self.__valeur_jet += self.__liste_des[i].lancer_le_des()
