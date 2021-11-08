484a8e6c9a81ad2a06eb02b101cc2f3aa4270d63
evainmanon <evain.manon@orange.fr>
2021-11-08 17:15:07 +0100

    DAO cellule

--- web/service/salle_service.py
+++ web/service/salle_service.py
@@ -6,3 +6,5 @@ class SalleService():
     @staticmethod
     def add_salle(id_donjon : int, coordonnees_salle_x : int, coordonnees_salle_y : int, salle : Salle): 
         SalleDAO.add_salle(id_donjon, coordonnees_salle_x , coordonnees_salle_y, salle)
+        for i in range(salle.coordonnees_salle):
+            None
