3abc78e0808ae06601e7ba8218f4282470807fce
Your Name <you@example.com>
2021-11-08 19:12:53 +0100

    type

--- client/view/maitre_du_jeu_view.py
+++ client/view/maitre_du_jeu_view.py
@@ -52,33 +52,38 @@ class MenuMJ(AbstractView):
 
     def make_choice(self):
         reponse = prompt(self.__questions)
-        if reponse['choix'] == 'Ajouter ou supprimer une entité':
-            message = input("Voulez-vous ajouter une entité à votre campagne ? \n Saisissez Oui ou Non")
-            if message == "Non":
-                return(SupprEntiView(self.joueur))
+        
+        if reponse['choix'] == 'Ajouter une entité':
+            from ajout_view.ajout_enti_view import AjoutEntiView
+            return AjoutEntiView(self.joueur)
             
-            if message == "Oui":
-                mes = input("Voulez-vous ajouter un personnage joueur ? \n Saisissez Oui ou Non")
-                if mes == "Oui":
-                    identifiant_entite = input("Saisissez l'identifiant du personnage à ajouter.")
-                    MJService.ajouter_entite(identifiant_entite) # Peut-être demander le nom pour un joueur
-                    from client.view.maitre_du_jeu_view import MenuMJ
-                    return MenuMJ(self.joueur,self.campagne)
-                if mes == "Non":
-                    mess = input("Voulez-vous ajouter un personnage non-joueur ? \n \n Saisissez Oui ou Non")
-                    if mess == "Oui":
-                        identifiant_entite = input("Saisissez l'identifiant du pnj à ajouter.")
-                        MJService.ajouter_entite(identifiant_entite)
-                        from client.view.maitre_du_jeu_view import MenuMJ
-                        return MenuMJ(self.joueur,self.campagne)
-                    else:
-                        identifiant_entite = input("Saisissez l'identifiant du monstre à ajouter.")
-                        MJService.supprimer_entite(identifiant_entite)
-                        from client.view.maitre_du_jeu_view import MenuMJ
-                        return MenuMJ(self.joueur,self.campagne)    
-            else:
-                from client.view.maitre_du_jeu_view import MenuMJ
-                return MenuMJ(self.joueur,self.campagne)   
+        if reponse['choix'] == 'Supprimer une entité':
+            from suppr_view.suppr_enti_view import SupprEntiView
+            return SupprEntiView(self.joueur)
+        reponse = prompt(self.__questions)
+    
+        # if message == "Oui":
+        #     mes = input("Voulez-vous ajouter un personnage joueur ? \n Saisissez Oui ou Non")
+        #     if mes == "Oui":
+        #             identifiant_entite = input("Saisissez l'identifiant du personnage à ajouter.")
+        #             MJService.ajouter_entite(identifiant_entite) # Peut-être demander le nom pour un joueur
+        #             from client.view.maitre_du_jeu_view import MenuMJ
+        #             return MenuMJ(self.joueur,self.campagne)
+        #     if mes == "Non":
+        #         mess = input("Voulez-vous ajouter un personnage non-joueur ? \n \n Saisissez Oui ou Non")
+        #         if mess == "Oui":
+        #                 identifiant_entite = input("Saisissez l'identifiant du pnj à ajouter.")
+        #                 MJService.ajouter_entite(identifiant_entite)
+        #                 from client.view.maitre_du_jeu_view import MenuMJ
+        #                 return MenuMJ(self.joueur,self.campagne)
+        #             else:
+        #                 identifiant_entite = input("Saisissez l'identifiant du monstre à ajouter.")
+        #                 MJService.supprimer_entite(identifiant_entite)
+        #                 from client.view.maitre_du_jeu_view import MenuMJ
+        #                 return MenuMJ(self.joueur,self.campagne)    
+        #     else:
+        #         from client.view.maitre_du_jeu_view import MenuMJ
+        #         return MenuMJ(self.joueur,self.campagne)   
         
         if reponse['choix'] == 'Créer un donjon':
             nom_donjon = input("Saisissez le nom du donjon à créer.")
--- mainjules.py
+++ mainjules.py
@@ -1,7 +1,11 @@
 from client.service.monstre_service import MonstreService
+import requests as req
 
 #print(essai.__dict__())
 
 #M = MonstreService.ImportMonstreWeb('aboleth')
 #print(M)
 
+req = MonstreService.web_monstre(nom)
+req = requ.get('https://www.dnd5eapi.co/api/monsters/' + nom)
+d=req.json()
