a9b509fc3871f50d40b344bb2becf13408d1683c
Julien-Sklarik <julien.sklarik@eleve.ensai.fr>
2021-11-08 19:04:41 +0100

    h

---
+++ client/service/campagne_service.py
@@ -0,0 +1,16 @@
+from typing import List
+
+from utils.singleton import Singleton
+from objets_metier.feedback import Feedback
+from objets_metier.utilisateur import Utilisateur
+from web.dao.utilisateur_dao import UtilisateurDAO
+from web.dao.administrateur_dao import AdministrateurDAO
+from web.dao.feed_back_dao import FeedBackDAO
+from client.exceptions.utilisateur_introuvable_exception import UtilisateurIntrouvableException
+
+
+class CampagneService(metaclass = Singleton):
+
+    @staticmethod    
+    def sauvegarder(campagne): 
+        pass
