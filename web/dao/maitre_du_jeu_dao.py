ef058eaaf840e785a8d4968d5d712aba550c98a7
evainmanon <evain.manon@orange.fr>
2021-11-13 13:42:51 +0100

    commit bis

--- web/dao/maitre_du_jeu_dao.py
+++ web/dao/maitre_du_jeu_dao.py
@@ -1,3 +1,4 @@
+from objets_metier.caracteristique import Caracteristique
 from objets_metier.personnage import Personnage
 from objets_metier.utilisateur import Utilisateur
 from web.dao.db_connection import DBConnection
@@ -11,10 +12,48 @@ class MjDAO:
         with DBConnection().connection as connection:
             with connection.cursor() as cursor:
                 cursor.execute(
-                    "SELECT username FROM UtilisateurCampagne "\
+                    "SELECT classe, race, lore, id_joueur, id_entite, nom_entite, force, experience, intelligence, charisme, dexterite, constitution, sagesse, vie, nom_attaque, nom_capacite, nom_langage "\
+                    "FROM Entite "\
+                    "JOIN Personnage ON Personnage.id_entite = Entite.id_entite"\
+                    "JOIN Capacite ON Entite.id_entite = Capacite.id_entite"\
+                    "JOIN Langage ON Entite.id_entite = Langage.id_entite"\
+                    "JOIN Attaque ON Entite.id_entite = Attaque.id_entite"\
+                    "JOIN Entite_Objet ON Entite.id_entite = Entite_Objet.id_entite"\
+                    "JOIN Objet ON Entite_Objet.id_objet = Objet.id_Objet"
                     "WHERE id_campagne = %(id_campagne)s;"\
                     , {"id_campagne" : id_campagne})
                 perso = cursor.fetchone()
+                nom_entite = [perso["id_entite"],perso["nom_entite"]]
+                force = [perso["id_entite"],perso["force"]]
+                experience = [perso["id_entite"],perso["experience"]]
+                intelligence = [perso["id_entite"],perso["intelligence"]]
+                charisme = [perso["id_entite"],perso["charisme"]]
+                dexterite = [perso["id_entite"],perso["dexterite"]]
+                constitution = [perso["id_entite"],perso["constitution"]]
+                vie = [perso["id_entite"],perso["vie"]]
+                sagesse = [perso["id_entite"],perso["sagesse"]]
+                
+                
+                
+                enti_description = [perso["id_entite"],perso["description"]]
+                classe_armure = [perso["id_entite"],perso["classe_armure"]]
+                classe = [perso["id_entite"],perso["classe"]]
+                race = [perso["id_entite"],perso["race"]]
+                lore = [perso["id_entite"],perso["lore"]]
+                id_entite = perso["id_entite"]
+                id_joueur = [perso["id_entite"],perso["id_joueur"]]
+
+                
+
+
+
+
+
+        liste_perso = []
+        for i in range(0, len(perso["username"])):
+            carac = Caracteristique(nom_entite = nom_entite, force = force, experience = experience, intelligence = intelligence, charisme = charisme, dexterite = dexterite, constitution = constitution, vie = vie, sagesse =  sagesse, attaques= attaques, capacites = capacites, languages = languages, description = description, classe_armure = classe_armure)
+            liste_perso.append(Personnage(classe, race, lore, id_joueur, id_entite, nom_entite, carac, objet))
+        return liste_perso
 #A finir 
         
 
