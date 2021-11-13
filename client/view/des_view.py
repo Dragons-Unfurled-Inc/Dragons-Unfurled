3ca754b8616eb7e9c9c082db0d4c156369adc8e9
LoicBomo <bomoloic@orange.fr>
2021-11-13 14:22:35 +0100

    Loic

--- client/view/des_view.py
+++ client/view/des_view.py
@@ -2,10 +2,10 @@ from PyInquirer import Separator, prompt
 
 from client.view.abstract_view import AbstractView
 from client.view.session import Session
-
+from objets_metier.joueur import Joueur
 class MenuDes(AbstractView):
 
-    def __init__(self):
+    def __init__(self,joueur,campagne):
         self.__questions = [
             {
                 'type': 'list',
@@ -22,6 +22,8 @@ class MenuDes(AbstractView):
                 ]
             }
         ]
+        self.joueur = joueur 
+        self.campagne = campagne
     
     def display_info(self):
         with open('client/dessins_ascii/border.txt', 'r', encoding="utf-8") as asset:
@@ -34,6 +36,11 @@ class MenuDes(AbstractView):
         if reponse['choix'] == 'Lancer librement des dés':
             pass
         if reponse['choix'] == 'Changer le mode de révélation des dés':
-            pass 
+            self.joueur.choix_revelation = not self.joueur.choix_revelation
         if reponse['choix'] == 'Quitter le menu de lancer de dés':
-            pass 
+            if  est_mj_campagne(self.joueur.identifiant):
+                from client.view.maitre_du_jeu_view import MenuMJ
+                return MenuMJ(self.joueur,self.campagne)
+            else:
+                from client.view.joueur_view import MenuJoueur
+                return MenuJoueur(self.joueur,self.campagne)    
