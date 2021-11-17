89722d7d328c4c0704e439489f3960623fc1258b
Julien-Sklarik <julien.sklarik@eleve.ensai.fr>
2021-11-17 16:28:47 +0100

    f

---
+++ client/vue/session.py
@@ -0,0 +1,15 @@
+from objets_metier.utilisateur import Utilisateur
+from utils.singleton import Singleton
+
+
+class Session(metaclass=Singleton):
+    def __init__(self):
+        """
+        Définition des variables que l'on stocke en session.
+        """
+        #self.est_administrateur = True
+        self.utilisateur = Utilisateur
+        
+        @staticmethod
+        def upgrade_util_jou():
+            self.utilisateur
