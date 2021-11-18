bfa8e694f27e0abb4af8a3554aa45d3fb30f24f7
Your Name <you@example.com>
2021-11-18 17:45:41 +0100

    env

--- web/web_config.py
+++ web/web_config.py
@@ -3,8 +3,8 @@ import os
 
 class WebConfig(metaclass=Singleton):
     def __init__(self) -> None:
-        api_port="localhost:5000"
-        output_dir="./generated/"
+        api_port=os.environ['API_URL']
+        output_dir=os.environ['FILE_OUTPUT_DIR']
         self.api_port=api_port
         self.output_dir=output_dir
         
