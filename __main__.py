89b1bd38c16eec753039ebf20653abcb78baf97f
Julien-Sklarik <julien.sklarik@eleve.ensai.fr>
2021-11-21 22:05:05 +0100

    c

--- __main__.py
+++ __main__.py
@@ -2,11 +2,12 @@ from client.vue.creation_compte_vue import CreaCompteVue
 #from client.vue.creation_personnage_vue import MenuPersonnage
 from client.vue.start_vue import StartVue
 from objets_metier.joueur import Joueur
-  
+
 if __name__ == '__main__':
+    
     # Lance notre première vue : StartVue
     current_vue = StartVue()
-    #current_vue = CreaCompteVue()
+
     with open('client/dessins_ascii/border.txt', 'r', encoding="utf-8") as affichage1,open('client/dessins_ascii/logo_moyen.txt', 'r', encoding="utf-8") as affichage2:
             print(affichage1.read(),affichage2.read())
             
