37e40d4f527a50efabbfba9d8fbc0c2ed279a6a6
evainmanon <evain.manon@orange.fr>
2021-11-23 23:06:56 +0100

    Consulter tous les donjon

--- web/dao/maitre_du_jeu_dao.py
+++ web/dao/maitre_du_jeu_dao.py
@@ -282,21 +282,171 @@ class MaitreDuJeuDAO:
                         res = cursor.fetchall()
                         id_cellules = [dict(row)["id_cellule"] for row in res] 
                 liste_objet = []
-                for id_cellule in id_cellules: 
+                liste_enti = []
+                for id_cell in id_cellules:
                     with DBConnection().connection as connection:
                         with connection.cursor() as cursor:
                             cursor.execute(
                                 "SELECT * "\
                                 "FROM Objet "\
                                 "WHERE (id_cellule = %(id_cellule)s) "\
-                                , {"id_cellule" : id_cellule})
-                            objet = cursor.fetchone()
-                    if objet == None:
-                        continue
-                    else:
-                        objet = [objet["id_objet"], objet["nom_objet"], objet["description_obj"]]
-                        liste_objet.append(Objet(id_objet = objet[0], nom_objet = objet[1], description = objet[2]))
-                liste_salle.append(Salle(id_salle = id_salle, nom_salle = salle[1][k], coordonnees_salle_donjon = [salle[2][k], salle[3][k]], objets = liste_objet))
+                                , {"id_cellule" : id_cell})
+                            objet = cursor.fetchall()
+                            if objet == None :
+                                id_objet = []
+                            else:
+                                id_objet = [objet[i]["id_objet"] for i in range(0, len(objet))]
+                    for i in range(0, len(id_objet)):
+                        liste_objet.append(Objet(id_objet = i, nom_objet = objet[i]["nom_objet"], description_obj = objet[i]["description_obj"]))
+                    with DBConnection().connection as connection:
+                        with connection.cursor() as cursor:
+                            cursor.execute(
+                                "SELECT id_entite "\
+                                "FROM Entite "\
+                                "WHERE (id_cellule = %(id_cellule)s) "\
+                                , {"id_cellule" : id_cell})
+                            id_enti = cursor.fetchall()
+                            if id_enti == None :
+                                id_entite = []
+                            else:
+                                id_entite = [id_enti[i]["id_objet"] for i in range(0, len(id_enti))]
+                    for id_ent in id_entite : 
+                        with DBConnection().connection as connection:
+                            with connection.cursor() as cursor:
+                                cursor.execute(
+                                    "SELECT * "\
+                                    "FROM Personnage "\
+                                    "WHERE (id_entite = %(id_entite)s) "\
+                                    , {"id_entite" : id_ent})
+                                perso = cursor.fetchone()
+                                if perso != None :
+                                    with DBConnection().connection as connection:
+                                        with connection.cursor() as cursor:
+                                            cursor.execute(
+                                                "SELECT * "\
+                                                "FROM Entite "\
+                                                "JOIN Utilisateur_Entite ON Entite.id_entite = Utilisateur_Entite.id_entite"\
+                                                "WHERE (id_entite = %(id_entite)s) "\
+                                                , {"id_entite" : id_ent})
+                                            enti_perso = cursor.fetchone()
+                                            cursor.execute(
+                                                "SELECT * "\
+                                                "FROM Capacite "\
+                                                "WHERE id_entite = %(id_entite)s "\
+                                                , {"id_entite" : id_entite})
+                                            capacite = cursor.fetchall()
+                                            if capacite == None : 
+                                                capacite = []
+                                            else:
+                                                capacite = [capacite[i]["nom_capacite"] for i in range(len(capacite))]
+                                            cursor.execute(
+                                                "SELECT * "\
+                                                "FROM Langage "\
+                                                "WHERE id_entite = %(id_entite)s"\
+                                                , {"id_entite" : id_entite})
+                                            langage = cursor.fetchall()
+                                            if langage == None:
+                                                langage =[]
+                                            else:
+                                                langage = [langage[i]["nom_langage"] for i in range(len(langage))]
+                                            cursor.execute(
+                                                "SELECT * "\
+                                                "FROM  Attaque "\
+                                                "WHERE id_entite = %(id_entite)s"\
+                                                , {"id_entite" : id_entite})
+                                            attaque = cursor.fetchall()
+                                            if attaque == None :
+                                                attaque = []
+                                            else : 
+                                                attaque = [attaque[i]["nom_attaque"] for i in range(0, len(attaque))]
+                                    with DBConnection().connection as connection:
+                                        with connection.cursor() as cursor:
+                                            cursor.execute(
+                                                "SELECT * "\
+                                                "FROM Entite_Objet "\
+                                                "JOIN Objet ON Entite_Objet.id_objet = Objet.id_objet"\
+                                                "WHERE (id_entite = %(id_entite)s) "\
+                                                , {"id_entite" : id_ent})
+                                            objet_perso = cursor.fetchall()
+                                            if objet_perso == None :
+                                                id_objet_perso = []
+                                            else:
+                                                id_objet_perso = [objet_perso[i]["id_objet"] for i in range(0, len(objet_perso))]
+                                    liste_objet_perso = [] 
+                                    for i in range(len(id_objet_perso)):
+                                        liste_objet_perso.append(Objet(id_objet = objet_perso[i]["id_objet"], nom_objet = objet_perso[i]["nom_objet"], description_obj = objet_perso[i]["description_obj"]))
+                                    caract = Caracteristique(nom_entite = enti_perso["nom_entite"], force = enti_perso["force"], experience = enti_perso["experience"], intelligence = enti_perso["intelligence"], charisme = enti_perso["charisme"], dexterite = enti_perso["dexterite"], constitution = enti_perso["constitution"], vie = enti_perso["vie"], sagesse =  enti_perso["sagesse"], attaques= attaque, capacites = capacite, languages = langage, description = enti_perso["description"], classe_armure = enti_perso["classe_armure"])
+                                    perso = Personnage(classe = perso["classe"], race = perso["race"], lore = perso["lore"], id_joueur = enti_perso["username"], id_entite = enti_perso["id_entite"], nom_entite = enti_perso["nom_entite"], caracteristiques_entite =  caract, objets = liste_objet_perso)
+                                    liste_enti.append(perso)
+
+                                else : 
+                                    with DBConnection().connection as connection:
+                                        with connection.cursor() as cursor:
+                                            cursor.execute(
+                                            "SELECT * "\
+                                            "FROM Monstre "\
+                                            "WHERE (id_entite = %(id_entite)s) "\
+                                            , {"id_entite" : id_ent})
+                                            perso = cursor.fetchone()
+                                    with DBConnection().connection as connection:
+                                        with connection.cursor() as cursor:
+                                            cursor.execute(
+                                                "SELECT * "\
+                                                    "FROM Entite "\
+                                                    "JOIN Utilisateur_Entite ON Entite.id_entite = Utilisateur_Entite.id_entite"\
+                                                    "WHERE (id_entite = %(id_entite)s) "\
+                                                    , {"id_entite" : id_ent})
+                                            enti_monstre = cursor.fetchone()
+                                            cursor.execute(
+                                                "SELECT * "\
+                                                "FROM Capacite "\
+                                                "WHERE id_entite = %(id_entite)s "\
+                                                , {"id_entite" : id_entite})
+                                            capacite = cursor.fetchall()
+                                            if capacite == None : 
+                                                capacite = []
+                                            else:
+                                                capacite = [capacite[i]["nom_capacite"] for i in range(len(capacite))]
+                                            cursor.execute(
+                                                "SELECT * "\
+                                                "FROM Langage "\
+                                                "WHERE id_entite = %(id_entite)s"\
+                                                , {"id_entite" : id_entite})
+                                            langage = cursor.fetchall()
+                                            if langage == None:
+                                                langage =[]
+                                            else:
+                                                langage = [langage[i]["nom_langage"] for i in range(len(langage))]
+                                            cursor.execute(
+                                                "SELECT * "\
+                                                "FROM  Attaque "\
+                                                "WHERE id_entite = %(id_entite)s"\
+                                                , {"id_entite" : id_entite})
+                                            attaque = cursor.fetchall()
+                                            if attaque == None :
+                                                attaque = []
+                                            else : 
+                                                attaque = [attaque[i]["nom_attaque"] for i in range(0, len(attaque))]
+                                    with DBConnection().connection as connection:
+                                        with connection.cursor() as cursor:
+                                            cursor.execute(
+                                                "SELECT * "\
+                                                "FROM Entite_Objet "\
+                                                "JOIN Objet ON Entite_Objet.id_objet = Objet.id_objet"\
+                                                "WHERE (id_entite = %(id_entite)s) "\
+                                                , {"id_entite" : id_ent})
+                                            objet_monstre = cursor.fetchall()
+                                            if objet_monstre == None :
+                                                id_objet_monstre = []
+                                            else:
+                                                id_objet_monstre = [objet_monstre[i]["id_objet"] for i in range(0, len(objet_monstre))]
+                                    liste_objet_monstre = [] 
+                                    for i in range(len(id_objet_monstre)):
+                                        liste_objet_monstre.append(Objet(id_objet = objet_monstre[i]["id_objet"], nom_objet = objet_monstre[i]["nom_objet"], description_obj = objet_monstre[i]["description_obj"]))
+                                    caract = Caracteristique(nom_entite = enti_monstre["nom_entite"], force = enti_monstre["force"], experience = enti_monstre["experience"], intelligence = enti_monstre["intelligence"], charisme = enti_monstre["charisme"], dexterite = enti_monstre["dexterite"], constitution = enti_monstre["constitution"], vie = enti_monstre["vie"], sagesse =  enti_monstre["sagesse"], attaques= attaque, capacites = capacite, languages = langage, description = enti_monstre["description"], classe_armure = enti_monstre["classe_armure"])
+                                    monstre = Monstre(type = monstre["type"], id_joueur = enti_monstre["username"], id_entite = enti_monstre["id_entite"], nom_entite = enti_monstre["nom_entite"], caracteristiques_entite =  caract, objets = liste_objet_monstre)
+                                    liste_enti.append(monstre)
+                liste_salle.append(Salle(id_salle = id_salle, nom_salle = salle[1][k], coordonnees_salle_donjon = [salle[2][k], salle[3][k]], objets = liste_objet, entites = liste_enti))
             liste_donjon.append(Donjon(id_donjon = donjon["id_donjon"], nom_donjon = donjon["nom_donjon"], pieces = liste_salle))
         return liste_donjon
 
